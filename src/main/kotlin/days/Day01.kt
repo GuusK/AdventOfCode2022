b0381a026c0c67d1d34bc4527d84e4ad06cc3fee
GuusK <guus.public@gmail.com>
2022-12-02 15:55:10 +0100

    Refactor day 1 for better readability and make it more functional/idiomatic programming

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -3,35 +3,24 @@ package days
 import resources.InputReader
 
 object Day01 : DayInterface {
-    override val number: Int
+    override val dayNumber: Int
         get() = 1
 
-    private var totalCalories: List<Int> = listOf()
-
-    init {
-        val input = InputReader.getResourceLines(1)
-        var parsed: MutableList<List<Int>> = mutableListOf()
-
-        var tmp: MutableList<Int> = mutableListOf()
-        for (it in input) {
-            if ("".equals(it)) {
-                parsed += tmp
-                tmp = mutableListOf()
-            } else {
-                tmp += it.toInt()
+    private val totalCalories: List<Int> =
+        InputReader.getResourceString(1)
+            .split("\n\n")
+            .map { calorieLst ->
+                calorieLst
+                    .split("\n")
+                    .filter { x -> x.isNotEmpty() }
+                    .sumOf { posInt -> posInt.toInt() }
             }
-        }
-        // Because last empty line is skipped when reading
-        parsed += tmp
-
-        totalCalories = parsed.map { x -> x.sum() }
-    }
 
     override fun part1(): Any {
         return totalCalories.maxOrNull() ?: -1
     }
 
     override fun part2(): Any {
-        return totalCalories.sorted().takeLast(3).sum()
+        return totalCalories.sortedDescending().take(3).sum()
     }
 }
