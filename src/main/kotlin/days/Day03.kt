bf03ded714320c76b281c565b4cfd473e5fc229e
GuusK <guus.public@gmail.com>
2022-12-03 06:49:25 +0100

    Refactor day 3

--- src/main/kotlin/days/Day03.kt
+++ src/main/kotlin/days/Day03.kt
@@ -8,10 +8,10 @@ object Day03 : DayInterface {
 
     private fun charToPriority(char: Char): Int {
         return if (char.isUpperCase()) {
-            26
+            27
         } else {
-            0
-        } + char.lowercaseChar().code - 96
+            1
+        } + (char.lowercaseChar() - 'a')
     }
 
     val input = InputReader.getResourceLines(3)
@@ -20,11 +20,9 @@ object Day03 : DayInterface {
         var score = 0
         for (line in input) {
             val firstHalf = line.subSequence(0, line.length / 2).toSet()
-            val secondHalf = line.subSequence(line.length / 2, line.length)
-            val matchingChar = secondHalf.map { char -> Pair(char, firstHalf.contains(char)) }
-                .filter { tup -> tup.second }
-                .first().first
-            score += charToPriority(matchingChar)
+            val secondHalf = line.subSequence(line.length / 2, line.length).toSet()
+            val matchingChar = firstHalf.intersect(secondHalf)
+            score += charToPriority(matchingChar.first())
         }
         return score
     }
@@ -32,8 +30,8 @@ object Day03 : DayInterface {
     override fun part2(): Any {
         var score = 0
         for (idx in input.indices step 3) {
-            val rucksacks = input.subList(idx, idx + 3).map { line -> line.toSet() }
-            val matchingChar = rucksacks[0].intersect(rucksacks[1]).intersect(rucksacks[2]).first()
+            val (a,b,c) = input.subList(idx, idx + 3).map { line -> line.toSet() }
+            val matchingChar = a.intersect(b).intersect(c).first()
             score += charToPriority(matchingChar)
         }
         return score
