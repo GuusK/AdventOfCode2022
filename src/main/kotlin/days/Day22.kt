aafe6ff4666f3df57ed391574da12aa08c7103cb
Guus Klinkenberg <ineedanemailforlicensestuff@gmail.com>
2023-12-02 14:15:27 +0100

    Solved day 22 part 1

--- src/main/kotlin/days/Day22.kt
+++ src/main/kotlin/days/Day22.kt
@@ -2,6 +2,7 @@ package days
 
 import days.day22.Facing
 import days.day22.Instruction
+import days.day22.MonkeyMapState
 import days.day22.Space
 import resources.InputReader
 import util.Point
@@ -13,12 +14,15 @@ object Day22 : DayInterface {
     val map: Map<Point, Space>
     val instrs: List<Instruction>
 
+    val path: MutableMap<Point, Facing> = mutableMapOf()
+
     init {
-        val input = InputReader.getResourceString("./input/day22example.txt")
-        val splitted = input.split("\n\n")
+        val linesep = System.getProperty("line.separator")
+//        val input = InputReader.getResourceString("./input/day22example.txt")
+        val input = InputReader.getResourceString(22)
+        val splitted = input.split(linesep + linesep)
         map = parseMap(splitted[0])
         instrs = Instruction.listFromString(splitted[1])
-
     }
 
     fun parseMap(input: String): Map<Point, Space> {
@@ -41,48 +45,60 @@ object Day22 : DayInterface {
         val maxX = map.maxBy { it.key.x }.key.x
         val maxY = map.maxBy { it.key.y }.key.y
 
+        println("=========================================================")
         for (y in 0..maxY) {
             for (x in 0..maxX) {
                 val p = Point(x, y)
-                print(map.getOrDefault(p, Space.Empty).char)
+                if(path.contains(p)){
+                    print(path[p]!!.char)
+                } else {
+                    print(map.getOrDefault(p, Space.Empty).char)
+                }
             }
             print('\n')
         }
+        println("=========================================================")
     }
 
     fun nextPosition(position: Point, facing: Facing): Point {
         val posNext = when (facing) {
-            Facing.Right -> position + Point(1, 0)
-            Facing.Down -> position + Point(-1, 0)
             Facing.Left -> position + Point(-1, 0)
-            Facing.Up -> position + Point(1, 0)
+            Facing.Right -> position + Point(1, 0)
+            // Map works that going down has increasing y index, as top row is index 0\
+            Facing.Down -> position + Point(0, 1)
+            Facing.Up -> position + Point(0, -1)
         }
         return if (map.containsKey(posNext)) {
             posNext
         } else {
             // we need to wrap around
             when (facing) {
-                Facing.Up -> Point(map.filter { it.key.x == position.x }.maxBy { it.key.x }.key.x, position.y)
-                Facing.Right -> Point(map.filter { it.key.y == position.y }.minBy { it.key.y }.key.y, position.x)
-                Facing.Down -> Point(map.filter { it.key.x == position.x }.minBy { it.key.x }.key.x, position.y)
-                Facing.Left -> Point(map.filter { it.key.y == position.y }.maxBy { it.key.y }.key.y, position.x)
+                Facing.Up -> Point(position.x, map.filter { it.key.x == position.x }.maxBy { it.key.y }.key.y)
+                Facing.Down -> Point(position.x, map.filter { it.key.x == position.x }.minBy { it.key.y }.key.y)
+
+                Facing.Right -> Point(map.filter { it.key.y == position.y }.minBy { it.key.x }.key.x, position.y)
+                Facing.Left -> Point(map.filter { it.key.y == position.y }.maxBy { it.key.x }.key.x, position.y)
             }
         }
     }
 
     fun walkMap(begin: Point): Pair<Point, Facing> {
+        println("Start walking at $begin")
         var facing: Facing = Facing.Right
         var position: Point = begin
+        path.put(position, facing)
         for (instr in instrs) {
             for (step in 0 until instr.distance) {
                 val nextPos = nextPosition(position, facing)
                 if (map[nextPos] == Space.Wall) {
                     // position not possible, so we abort stepping further
-                    continue
+                    break
                 }
                 position = nextPos
-                facing = facing.rotate(instr.rotation)
+                path.put(position, facing)
+//                printMap(map)
             }
+            facing = facing.rotate(instr.rotation)
         }
         return Pair(position, facing)
     }
@@ -90,13 +106,15 @@ object Day22 : DayInterface {
 
     override fun part1(): Any {
         val beginX = map
-            .filter { it.key.y == 0 }
-            .maxBy { it.key.x }
+            .filter { it.key.y == 0 && it.value.char == '.'}
+            .minBy { it.key.x }
             .key.x
         val begin = Point(beginX, 0)
+//        printMap(map)
         val res = walkMap(begin)
+        print("Finished walking at $res")
 
-        return (res.first.x + 1) * 1000 + (res.first.y + 1) * 4 + res.second.worth
+        return (res.first.y + 1) * 1000 + (res.first.x + 1) * 4 + res.second.worth
     }
 
     override fun part2(): Any {
--- src/main/kotlin/days/day22/Facing.kt
+++ src/main/kotlin/days/day22/Facing.kt
@@ -2,16 +2,16 @@ package days.day22
 
 import java.lang.IllegalArgumentException
 
-enum class Facing(val worth: Int) {
-    Right(0),
-    Down(1),
-    Left(2),
-    Up(3);
-    
-    fun rotate(char: Char) : Facing {
+enum class Facing(val worth: Int, val char: Char) {
+    Right(0, '>'),
+    Down(1, 'v'),
+    Left(2, '<'),
+    Up(3, '^');
+
+    fun rotate(char: Char): Facing {
         val values = enumValues<Facing>()
         val index = values.indexOf(this)
-        return when(char){
+        return when (char) {
             'R' -> values[(index + 1) % values.size]
             'L' -> values[Math.floorMod(index - 1, values.size)]
             else -> throw IllegalArgumentException("Illegal rotation")
